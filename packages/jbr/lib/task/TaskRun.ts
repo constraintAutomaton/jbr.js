bf455de0ac10d888fa98350fa8e60bff618140f2
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-03-21 09:10:22 +0100

    try catch added for the running of experiment

--- packages/jbr/lib/task/TaskRun.ts
+++ packages/jbr/lib/task/TaskRun.ts
@@ -20,14 +20,17 @@ export class TaskRun {
     await ExperimentLoader.requireExperimentPrepared(this.context.experimentPaths.root);
     const { experiments, experimentPathsArray } = await (await ExperimentLoader.build(this.context.mainModulePath))
       .instantiateExperiments(this.context.experimentName, this.context.experimentPaths.root);
-    for (const [ i, experiment ] of experiments.entries()) {
+    for (const [i, experiment] of experiments.entries()) {
       if (this.combination === undefined || this.combination === i) {
         // Log status
         if (experiments.length > 1) {
           this.context.logger.info(`🧩 Running experiment combination ${i}`);
         }
-
-        await experiment.run({ ...this.context, experimentPaths: experimentPathsArray[i] });
+        try {
+          await experiment.run({ ...this.context, experimentPaths: experimentPathsArray[i] });
+        } catch (error) {
+          this.context.logger.error(error);
+        }
       }
     }
   }
